0d2ba911c1ed2103e391a3f1902055f8c21ccf66
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2021-04-20 17:57:30 +0300

    Allow congiguration of minimal level

---
+++ common.py
@@ -0,0 +1,42 @@
+
+## todo: not sure if this should be done here or if it would be smarter somehwere else to allow for different configurations
+
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+def arguments():
+
+    import argparse
+
+    parser = argparse.ArgumentParser( description="Automatically tag pictures using exernal APIs." )
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    parser.add_argument(
+        "--secrets",
+        type=str,
+        default="./secrets.yaml",
+        help="Path to the configuration file",
+    )
+
+    ## todo: implement me
+    group.add_argument(
+        "--file",
+        type=str,
+        help="Path to file containing URLs"
+    )
+
+    group.add_argument(
+        "--folder",
+        type=str,
+        help="Path to folder containing images"
+    )
+
+    parser.add_argument(
+        "--output",
+        default="./output/",
+        type=str,
+        help="folder where the pictures will be stored",
+    )
+
+    return parser.parse_args()
