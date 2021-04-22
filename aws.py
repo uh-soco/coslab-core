7d8ce63f84b1f3b0ad53588b489b3feb564927b3
berginator <mrantonberg@gmail.com>
2021-04-22 18:30:11 +0300

    db implementation

--- aws.py
+++ aws.py
@@ -2,47 +2,69 @@ import boto3
 import json
 import yaml
 import os
-
 import common
+import sqlite3
+import datetime
 
-def process_local( client, images ):
+conn = sqlite3.connect("results.db")
+db = conn.cursor()
+db.execute(
+    "CREATE TABLE IF NOT EXISTS results (id INTEGER PRIMARY KEY, image TEXT, label TEXT, label_num INT, service TEXT, confidence REAL, date DATE )"
+)
+
+
+def process_local(client, images):
 
     holder_responses = []
     holder_labels = []
 
     for imageFile in images:
-
-        image =  open(imageFile, "rb")
-
-        ## todo: check if it is cheaper to label several images at the same time or not
+        image = open(imageFile, "rb")
         content = image.read()
-
         response = client.detect_labels(
-            Image= {"Bytes": content} ,
-            MinConfidence = common.config.getfloat('DEFAULT', 'minimal_confidence')
+            Image={"Bytes": content},
+            MinConfidence=common.config.getfloat("DEFAULT", "minimal_confidence"),
         )
-
         ## todo: save responses directly to database/json output
-        holder_responses.append( response )
+        holder_responses.append(response)
+        # print("Detected labels for " + imageFile)
 
-        print("Detected labels for " + imageFile)
+        for label_counter, label in enumerate(response["Labels"]):
+            service = "aws"
+            image_id = imageFile
+            label_num = label_counter
+            label = label["Name"]
+            confidence = label["Confidence"]
+            date = datetime.datetime.now()
 
-        for label_counter, label in enumerate( response["Labels"] ):
-            temp_dict = {}
-            temp_dict["image_id"] = imageFile
-            temp_dict["label_num"] = label_counter
-            temp_dict["label"] = label["Name"]
-            temp_dict["confidence"] = label["Confidence"]
-
-            holder_labels.append(temp_dict)
+            # Saving to database
+            db.executemany(
+                """INSERT INTO results (image,label,label_num,service,confidence,date) VALUES(?,?,?,?,?,?)""",
+                image_id,
+                label,
+                label_num,
+                service,
+                confidence,
+                date,
+            )
+            conn.commit()
+            conn.close()
+            # temp_dict = {}
+            # temp_dict["image_id"] = imageFile
+            # temp_dict["label_num"] = label_counter
+            # temp_dict["label"] = label["Name"]
+            # temp_dict["confidence"] = label["Confidence"]
+            # holder_labels.append(temp_dict)
 
     ## todo: save to SQL?
-    print( holder_labels )
 
-if __name__ == '__main__':
-    args = common.arguments() ## creates a common parameters sets for all programs
+    # print(holder_labels)
 
-    secrets = yaml.safe_load( open( args.secrets ) )
+
+if __name__ == "__main__":
+    args = common.arguments()  ## creates a common parameters sets for all programs
+
+    secrets = yaml.safe_load(open(args.secrets))
 
     api_id = secrets["aws"]["api_id"]
     api_key = secrets["aws"]["api_key"]
@@ -57,5 +79,5 @@ if __name__ == '__main__':
 
     if args.folder:
         directory = args.folder
-        images = [ os.path.join(directory, file) for file in os.listdir(directory) ]
-        process_local( client, images )
+        images = [os.path.join(directory, file) for file in os.listdir(directory)]
+        process_local(client, images)
