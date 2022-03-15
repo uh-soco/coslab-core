25fc2ad91143ff7d85d2fbc5934c53ab4b66a399
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2022-03-15 10:51:44 +0200

    Refactor code to ensure models are consistently.

--- src/imagetaggers/tag_comparator.py
+++ src/imagetaggers/tag_comparator.py
@@ -1,36 +1,26 @@
 from itertools import permutations
 import collections
-
-import gensim.downloader as api
-from gensim.models.word2vec import Word2Vec
-from gensim.models import KeyedVectors
 import sqlite3
+
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy import spatial
 
-# load models
-
-glove_model = api.load("glove-wiki-gigaword-50") #choose more models from https://github.com/RaRe-Technologies/gensim-data
 
 ## TODO: make this right and working with real package structure as well
+
 import os
 datapath = os.path.dirname(os.path.realpath(__file__)) + '/../../trained_vectordata/'
 
-filename = 'GoogleNews-vectors-negative300.bin'
-
-w2v_model = KeyedVectors.load_word2vec_format( datapath + filename, binary=True)
-
-#################Bert######################
-from sentence_transformers import SentenceTransformer, util
-bert_model = SentenceTransformer('paraphrase-MiniLM-L12-v2')
-
 # Comparator template
-def identity_comparator( tag1, tag2 ):
 
+def identity_comparator( tag1, tag2 ):
     return float( tag1 == tag2 )
 
 # Glove comparator
+
+from scipy import spatial
+import gensim.downloader as gensimdl
+
+glove_model = gensimdl.load("glove-wiki-gigaword-50") #choose more models from https://github.com/RaRe-Technologies/gensim-data
 
 def _preprocess(s):
     return [i.lower() for i in s.split()]
@@ -50,14 +40,23 @@ def glove_comparator( tag1, tag2 ):
 
 # Word2Vec comparator
 
-def w2v_comparator( tag1, tag2 ):
+from gensim.models.word2vec import Word2Vec
+from gensim.models import KeyedVectors
 
+w2v_model = KeyedVectors.load_word2vec_format( datapath + 'GoogleNews-vectors-negative300.bin', binary=True)
+
+def w2v_comparator( tag1, tag2 ):
     try:
         return w2v_model.similarity(tag1, tag2)
     except KeyError:
         return -1
 
 # Bert comparator
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+from sentence_transformers import SentenceTransformer, util
+bert_model = SentenceTransformer('paraphrase-MiniLM-L12-v2')
 
 def bert_comparator( tag1, tag2 ):
 
